dcf60c34987fe8f81fc98914acd6b19ab57ad549
ghost <ghost>
2021-03-16 16:48:59 -0400

    manual aspect ratio scaling in js

--- gatherings/forest/common.js
+++ gatherings/forest/common.js
@@ -1,6 +1,15 @@
-// Add random hash to links and iframe src to allow arbitrary recursion
+// Run a couple quick hacks at the earliest callback possible (during dom parsing i think?)
 document.addEventListener('readystatechange', (event) => {
-  if (!document._alreadyRandomized) {
+  if (!document._alreadyRun) {
+    randomizeLinks();
+    fixAspectRatio();
+
+    document._alreadyRun = true;
+  }
+});
+
+// Add random query string to links and iframe src to allow arbitrary recursion
+randomizeLinks = () => {
     console.log(window.location.href, "randomize links");
     var links = Array.from(document.getElementsByClassName('hotspot'))
     links.forEach((el) => {
@@ -13,7 +22,28 @@ document.addEventListener('readystatechange', (event) => {
     iframes.forEach((el) => {
       el.src += '?r=' + Math.random()
     })
+}
 
-    document._alreadyRandomized = true;
+
+fixAspectRatio = () => {
+  // fix aspect ratio of .content element
+  let contentEl = document.querySelector('.content');
+
+  const setContentAspect = () => {
+    console.log("resize");
+    let style = getComputedStyle(contentEl);
+    let aspect = style.getPropertyValue("--aspect-ratio");
+    let fillFraction = style.getPropertyValue("--fill-fraction");
+
+    let maxWidth  = window.innerWidth *fillFraction;
+    let maxHeight = window.innerHeight*fillFraction;
+    let width = Math.min(maxHeight*aspect, maxWidth);
+    let height = Math.min(maxWidth*aspect, maxHeight);
+
+    contentEl.style.width  = width+"px";
+    contentEl.style.height = height+"px";
   }
-});
+
+  setContentAspect();
+  window.addEventListener('resize', setContentAspect);
+}
