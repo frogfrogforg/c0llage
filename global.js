8db02581774ad5c3e9796fbd165c59c6659c301b
ghost <ghost>
2021-03-19 21:47:30 -0400

    made a sketchy global state

--- global.js
+++ global.js
@@ -4,12 +4,38 @@ import { Events } from "./core/events.js"
 const root = window.top
 const keys = ["d", "b"]
 
+const initialState = {
+  flags: {}
+}
+
 // -- api --
 if (root.d == null) {
   // global obj for other shared modules
   const global = {
-    Events
+    Events,
   }
+  
+  const stateCache = JSON.parse((window.localStorage.getItem('state') || 'false'))  || initialState
+
+  // TODO: eventually this should raise events so that state changes can happen without reloading 
+  // State can get extremely complex with proxies and such, since we don't have any use case yet, I decided not to go the whole length
+  // Especially given how annoying it is to observe changes in a deeply nested object
+  // A cool way would be to have a very limited way on how state works
+  Object.defineProperty(global, 'State', {
+      configurable: true,
+      get() {
+        return stateCache
+      }
+    })
+
+    global.SaveState = function() {
+      console.log('saving state', stateCache)
+      window.localStorage.setItem('state', JSON.stringify(stateCache))
+    }
+
+    root.addEventListener('beforeunload', () => {
+      global.SaveState()
+    })
 
   // share it between every key
   for (const name of keys) {
