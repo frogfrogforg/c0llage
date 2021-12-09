3ecaafec0beb9a81f8c61a1a9639531e64571190
danny phantom <danny phantom>
2021-12-09 12:01:59 -0500

    i seem to have done the impossible

--- core/s-cript.js
+++ core/s-cript.js
@@ -76,8 +76,6 @@ class ScriptElement extends HTMLParsedElement {
   // -- commands --
   // show a dialog for a section name and item index
   showNamedDialog(name) {
-    // DEBUG
-    console.log("showing named dialogue bro");
     const m = this
 
     m.god.showNamedDialogForHerald(
@@ -237,8 +235,6 @@ class ScriptHerald {
 
   // bind the script to its target
   bindToTarget($window) {
-    // DEBUG
-    console.log("biiiiinding");
     const m = this
 
     // store the window
